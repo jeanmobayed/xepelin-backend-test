427466a8de97b9b7b02587fa91e3c8f9509b9cd0
Jean Pier Mobayed Kounboz <jeanmobayed@gmail.com>
2022-03-28 21:15:23 -0400

    fix: db sync

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,9 +7,9 @@ export default () => ({
         database: process.env.DB_NAME,
         password:  process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
-        username:  process.env.DB_USER,
+        username: process.env.DB_USER,
         entities: [path.resolve(__dirname, '..', '**/**.entity!(*.d).{ts,js}')],
-        synchronize: true,
+        synchronize: process.env.DB_SYNC === 'true',
         logging: true,
         port: parseInt(process.env.DB_PORT),
         extra: {
