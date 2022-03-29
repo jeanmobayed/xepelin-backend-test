c0789b75a50f464a0d6b27fdd6d6d03428f641c9
Jean Pier Mobayed Kounboz <jeanmobayed@gmail.com>
2022-03-28 20:57:36 -0400

    fix: enabling table sync

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,8 +9,8 @@ export default () => ({
         host: process.env.DB_HOST,
         username:  process.env.DB_USER,
         entities: [path.resolve(__dirname, '..', '**/**.entity!(*.d).{ts,js}')],
-        synchronize: process.env.NODE_ENV === 'local',
-        logging: process.env.NODE_ENV === 'local',
+        synchronize: true,
+        logging: true,
         port: parseInt(process.env.DB_PORT),
         extra: {
           decimalNumbers: true
